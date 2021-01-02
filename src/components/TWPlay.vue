b7a9f0712c31205248b4dc3606ffba6d1066d7a2
Stu Heiss <sheiss@highlandsolutions.com>
2021-01-02 16:11:12 -0600

    Create some demos
    App.vue just loads TWPlay.vue
    FormsDemo.vue displays the demo page for @tailwindcss/forms

---
+++ src/components/TWPlay.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="antialiased text-gray-900 px-6">
+    <div class="max-w-xl mx-auto py-12 md:max-w-4xl">
+      <!-- <p>FormsDemo</p>
+      <FormsDemo /> -->
+      <p>t-input</p>
+      <t-input
+      />
+      <p>t-button</p>
+      <t-button
+        @click="buttonClicked"
+      >
+        Open Modal
+      </t-button>
+      <t-modal
+        v-model="showModal"
+        header="Title of the modal"
+      >
+        Content of the modal.
+        <template v-slot:footer>
+          <div class="flex justify-between">
+            <t-button type="button">
+              Cancel
+            </t-button>
+            <t-button type="button">
+              Ok
+            </t-button>
+          </div>
+        </template>
+      </t-modal>
+    </div>
+  </div>
+</template>
+
+<script>
+import { TInput, TButton, TModal } from 'vue-tailwind/dist/components'
+// import FormsDemo from './FormsDemo.vue'
+
+export default {
+  name: 'TWPlay',
+  components: {
+    // FormsDemo,
+    TModal,
+    TInput,
+    TButton,
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    buttonClicked: function() {
+      this.showModal = !this.showModal;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
